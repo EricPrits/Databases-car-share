6697ec0acddfb1c8a606eca1632f2862b6ac3087
Bleef-Mcfloogan <13ajs11@queenu.ca>
2017-04-01 19:01:09 -0400

    member page ready for style, added child page links

--- car_share/member.php
+++ car_share/member.php
@@ -1,9 +1,24 @@
 <!DOCTYPE HTML>
 <html>
     <head>
+		<style>
+        table.content, th.content, td.content {
+        border: 1px solid black;
+        border-collapse: collapse;
+        }
+        th.content, td.content {
+            padding: 5px;
+            text-align: left;    
+                }
+        </style>
         <title>member</title>
+		<h1>WELCOME TO KTOWN CAR SHARE</h1>
     </head>
 <body>
+	<?php
+		session_start();
+	 ?>
+
 
    <?php
         if(isset($_POST['rentalHistoryButton'])){
@@ -46,10 +61,52 @@
             </td>
 			<td>
                 <input type='submit' id='reserveCarButton' name='reserveCarButton' value='Reserve Car' />
-            </td>
+            </td>	
         </tr>
     </table>
     </form>
+		<h2>Your Reservations</h2>
+	<td><?php
+                include_once 'config/connection.php'; 
+                $query = "SELECT rental_ID, reservation_number, date, make, model, year FROM reservations NATURAL JOIN cars WHERE 	member_number=?;";
+				$stmt = $con -> prepare($query); 
+				if (isset($_SESSION['member_number'])) {
+                    $temp_mem_num = $_SESSION['member_number'];
+                }
+                $stmt->bind_Param("s", $temp_mem_num);
+				//$stmt->bind_Param("s", $_SESSION['member_number']);
+         				
+                try {
+                $stmt -> execute(); }
+                catch(Exception $exception) {
+                echo "Query failed: ", $exception->getMessage(); }
+                $result = $stmt -> get_result();
+                //echo "<br />" . $result -> num_rows . " rows in result <br />";
+
+                echo '<table class="content" style="width:100%">
+                  <tr class="content">
+					<th class="content">reservation_number</th>
+                    <th class="content">date</th>
+                    <th class="content">make</th>
+                    <th class="content">model</th>
+                    <th class="content">year</th>
+					<th class="content">rental_ID</th>
+                  </tr>';
+                while($row = $result->fetch_assoc())
+                    {
+                         echo '<tr class="content">
+						        <td class="content">' . $row["reservation_number"] . '</td>
+                                <td class="content">' . $row["date"] . '</td>
+                                <td class="content">' . $row["make"] . '</td>
+                                <td class="content">' . $row["model"] . '</td>
+                                <td class="content">' . $row["year"] . '</td>
+								<td class="content">' . $row["rental_ID"] . '</td>
+                            </tr>';
+                    }
+                echo '</table>';
+                $result->close();
+                $con->close();
+            ?></td>
 	
 </body>
 </html>
