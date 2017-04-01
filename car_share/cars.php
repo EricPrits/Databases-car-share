ab99c5cffdf519587d568c0b5dc8ff03a7a92b12
Alex <alexjjseppala@gmail.com>
2017-04-01 16:27:37 -0400

    cars page completed minus styling

--- car_share/cars.php
+++ car_share/cars.php
@@ -1,6 +1,16 @@
 <!DOCTYPE HTML>
 <html>
     <head>
+        <style>
+        table.content, th.content, td.content {
+        border: 1px solid black;
+        border-collapse: collapse;
+        }
+        th.content, td.content {
+            padding: 5px;
+            text-align: left;    
+                }
+        </style>
         <title>cars</title>
     </head>
 <body>
@@ -50,5 +60,40 @@
         </tr>
     </table>
     </form>
+    <td><?php
+                include_once 'config/connection.php'; 
+                $query = "SELECT * FROM cars;";
+                $stmt = $con -> prepare($query); 
+                try {
+                $stmt -> execute(); }
+                catch(Exception $exception) {
+                echo "Query failed: ", $exception->getMessage(); }
+                $result = $stmt -> get_result();
+                //echo "<br />" . $result -> num_rows . " rows in result <br />";
+
+                echo '<table class="content" style="width:100%">
+                  <tr class="content">
+                    <th class="content">VIN</th>
+                    <th class="content">Make</th>
+                    <th class="content">Model</th>
+                    <th class="content">Year</th>
+                    <th class="content">Location ID</th>
+                    <th class="content">Daily Fee</th>
+                  </tr>';
+                while($row = $result->fetch_assoc())
+                    {
+                         echo '<tr class="content">
+                                <td class="content">' . $row["VIN"] . '</td>
+                                <td class="content">' . $row["make"] . '</td>
+                                <td class="content">' . $row["model"] . '</td>
+                                <td class="content">' . $row["year"] . '</td>
+                                <td class="content">' . $row["location_ID"] . '</td>
+                                <td class="content">' . $row["daily_fee"] . '</td>
+                            </tr>';
+                    }
+                echo '</table>';
+                $result->close();
+                $con->close();
+            ?></td>
 </body>
 </html>
