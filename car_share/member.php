626aedf54bb2457f6054e30262d264aede086b1c
Bleef-Mcfloogan <13ajs11@queenu.ca>
2017-04-01 16:52:44 -0400

    member_page

--- car_share/member.php
+++ car_share/member.php
@@ -11,14 +11,45 @@
         exit;
 }
  ?>
+ 
+    <?php
+        if(isset($_POST['pickupFormButton'])){
+        header('Location: pickupForm.php');
+        exit;
+}
+ ?>
+ 
+	<?php
+        if(isset($_POST['dropoffFormButton'])){
+        header('Location: dropoffForm.php');
+        exit;
+}
+ ?>
+ 
+	<?php
+        if(isset($_POST['reserveCarButton'])){
+        header('Location: reserveCar.php');
+        exit;
+}
+ ?>
 
     <form name='member' id='member' action='member.php' method='post'>
     <table border='0'>
             <td>
                 <input type='submit' id='rentalHistoryButton' name='rentalHistoryButton' value='Rental History' />
             </td>
+			<td>
+                <input type='submit' id='pickupFormButton' name='pickupFormButton' value='Pick Up Car' />
+            </td>
+			<td>
+                <input type='submit' id='dropoffFormButton' name='dropoffFormButton' value='Drop Off Car' />
+            </td>
+			<td>
+                <input type='submit' id='reserveCarButton' name='reserveCarButton' value='Reserve Car' />
+            </td>
         </tr>
     </table>
     </form>
+	
 </body>
 </html>
