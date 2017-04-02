ba73e00aa6b9f0560eeb658ac3488b59dd63c5cb
Bleef-Mcfloogan <13ajs11@queenu.ca>
2017-04-02 13:36:17 -0400

    addes some buttons to member pages

--- car_share/dropoffForm.php
+++ car_share/dropoffForm.php
@@ -1,7 +1,27 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
+	<title>DropoffFormPage</title>
 	</head>
 	<body>
+	<?php
+			//Create a user session or resume an existing one
+			session_start();
+			?>
+			
+			<?php
+			if(isset($_POST['backButton'])){
+			header('Location: member.php');
+			exit;
+			}
+		?>
+		
+		<form name='memberRentalHistory' id='memberRentalHistory' action='memberRentalHistory.php' method='post'>
+			<table border='0'>
+				<td>
+                <input type='submit' id='backButton' name='backButton' value='Back' />
+				</td>
+			</table>
+		</form>
 	</body>
 </html>
